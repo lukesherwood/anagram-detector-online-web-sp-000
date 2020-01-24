d47470025ebaae2d9063400502a9f89ba43e2068
Luke Sherwood <luke.sherwood55@gmail.com>
2020-01-24 23:21:07 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :anagram
+def initialize(anagram)
+  @anagram = anagram
+end
+  
+def match
+  #recieve Array
+  #iterate thru array to compare with anagram
+  #split both words (one from array + anagram) into individual letters
+  #sort letters and compare to anagram
+  #return (all) match(es)
+end
+  
+end
